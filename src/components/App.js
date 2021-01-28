3473d7f04ad9c0c6b514492180c5ab76fb2e0dc5
mephasto <nicdakka@gmail.com>
2021-01-28 16:43:08 -0300

    Render fetched posts. Styles added for posts list navigation.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,58 @@
 import React from 'react'
 import Moment from 'react-moment'
-import { connect } from 'react-redux';
-import * as actions from '../actions';
+import { connect } from 'react-redux'
+import * as actions from '../actions'
+import '../css/styles.css'
 
 class App extends React.Component {
     componentDidMount() {
         this.props.dispatch(actions.fetchPosts())
     }
-
+    
     render() {
-        return("Hello World!")
+        const { posts } = this.props
+        return(
+            <React.Fragment>
+                <div className="posts_nav">
+                    <h1 className="nav_header">Reddit Posts</h1>
+                    <ul className="nav_posts_list">
+                    {posts.map((post, index) => (
+                        <li className="post" id={post.data.id}>
+                            <div className="post_header">
+                                <div className="post_state"></div>
+                                <div className="post_author">{post.data.author}</div>
+                                <div className="post_age">18 hours ago</div>
+                            </div>
+                            <div className="post_body">
+                                {post.data.thumbnail === 'self' || post.data.thumbnail === 'default' ? (
+                                    ''
+                                ) : (
+                                    <div className="thumbnail_placeholder">
+                                        <img src={post.data.thumbnail} title={post.data.title}></img>
+                                    </div>
+                                )}
+                                <p className="title">{post.data.title}</p>
+                            </div>
+                            <div className="post_footer">
+                                <div className="post_dismiss">
+                                    <div className="dismiss_icon">x</div>
+                                    <span className="dismiss_label">Dismiss Post</span>
+                                </div>
+                                <div className="post_comments">
+                                    <span className="count">{post.data.num_comments} comments</span>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                    </ul>
+                    <div className="nav_footer">
+                        <div className="post_dismiss_all">
+                            <span className="dismiss_label">Dismiss All Posts</span>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
     }
 }
 
@@ -19,4 +62,4 @@ const mapStateToProps = state => ({
 
 connect(mapStateToProps)
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App)
